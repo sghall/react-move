0c722d1f033816c2a3928affeb819e24b33b1a52
Steve Hall <hall.steven.g@gmail.com>
2017-08-23 21:36:33 -0700

    remove bundle inspector from docs build

--- docs/webpack.prd.config.js
+++ docs/webpack.prd.config.js
@@ -4,7 +4,6 @@ const path = require('path');
 const webpack = require('webpack');
 const webpackBaseConfig = require('./webpackBaseConfig');
 const dllManifest = require('./build/dll.manifest.json');
-const BundleInspector = require('bundle-inspector-webpack-plugin');
 
 module.exports = Object.assign({}, webpackBaseConfig, {
   context: path.resolve(__dirname),
@@ -71,6 +70,5 @@ module.exports = Object.assign({}, webpackBaseConfig, {
         NODE_ENV: JSON.stringify('production'),
       },
     }),
-    new BundleInspector(),
   ]),
 });
