6ae235d685ca1a0b6644252d748faa3059a4a34f
Steve Hall <hall.steven.g@gmail.com>
2017-10-23 17:34:04 -0700

    add toggle example

--- docs/src/routes/examples/Simple/index.js
+++ docs/src/routes/examples/Simple/index.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import CodeExample from 'docs/src/components/CodeExample';
+import Toggle from './Toggle';
 import Bars1 from './Bars1';
 import Bars2 from './Bars2';
 import Bars3 from './Bars3';
@@ -14,26 +15,32 @@ const Example = (props) => {
   return (
     <div>
       <CodeExample
+        code={exampleContext('./Simple/Toggle')}
+        title="Example 1: Basic Toggle"
+      >
+        <Toggle />
+      </CodeExample>
+      <CodeExample
         code={exampleContext('./Simple/Bars1')}
-        title="Example 1: Simple Bars"
+        title="Example 2: Simple Bars"
       >
         <Bars1 />
       </CodeExample>
       <CodeExample
         code={exampleContext('./Simple/Bars2')}
-        title="Example 2: Bars w/ More Complex Timing"
+        title="Example 3: Bars w/ More Complex Timing"
       >
         <Bars2 />
       </CodeExample>
       <CodeExample
         code={exampleContext('./Simple/Bars3')}
-        title="Example 3: Passing Arrays of Transitions"
+        title="Example 4: Passing Arrays of Transitions"
       >
         <Bars3 />
       </CodeExample>
       <CodeExample
         code={exampleContext('./Simple/Circles')}
-        title="Example 4: Namespacing State"
+        title="Example 5: Namespacing State"
       >
         <Circles />
       </CodeExample>
