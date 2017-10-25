2dd9cb2ac3b81197add8a7dc541f7679bfa2d632
Steve Hall <hall.steven.g@gmail.com>
2017-10-25 08:25:50 -0700

    add example

--- docs/src/routes/examples/Charts/index.js
+++ docs/src/routes/examples/Charts/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import CodeExample from 'docs/src/components/CodeExample';
 import DonutChart1 from './DonutChart1';
+import DonutChart2 from './DonutChart2';
 
 const Example = (props) => {
   const { route: { exampleContext } } = props;
@@ -15,6 +16,12 @@ const Example = (props) => {
         title="Example 1: Donut Chart"
       >
         <DonutChart1 />
+      </CodeExample>
+      <CodeExample
+        code={exampleContext('./Charts/DonutChart2')}
+        title="Example 2: Donut Chart w/ Entering/Leaving"
+      >
+        <DonutChart2 />
       </CodeExample>
     </div>
   );
