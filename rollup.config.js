be6eadfbc065b44f2f0c4a118aecf15dfaf041fd
Steve Hall <hall.steven.g@gmail.com>
2019-01-17 12:16:36 -0700

    update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,82 @@
-// @flow weak
+import babel from 'rollup-plugin-babel'
+import replace from 'rollup-plugin-replace'
+import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
+import { terser } from 'rollup-plugin-terser'
 
-import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
-import replace from 'rollup-plugin-replace';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
+import pkg from './package.json'
 
-const config = {
-  input: 'src/index.js',
-  name: 'react-move',
-  globals: {
-    react: 'React',
-  },
-  external: [
-    'react',
-  ],
-  plugins: [
-    babel({
-      exclude: 'node_modules/**',
-    }),
-    resolve(),
-    commonjs({
-      include: /node_modules/,
-    }),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-    }),
-  ],
-};
+const input = 'src/index.js'
+const globalName = 'ReactMove'
 
-if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(uglify());
+const globals = {
+  react: 'React',
 }
 
-export default config;
+const umd = [
+  {
+    input,
+    output: {
+      file: `build/dist/${pkg.name}.js`,
+      format: 'umd',
+      exports: 'named',
+      name: globalName,
+      globals,
+    },
+    external: Object.keys(globals),
+    plugins: [
+      babel({
+        runtimeHelpers: true,
+        exclude: 'node_modules/**',
+        presets: ['@babel/preset-react'],
+        plugins: [
+          '@babel/plugin-proposal-class-properties',
+          ['transform-react-remove-prop-types', { mode: 'wrap' }],
+          ['@babel/transform-runtime'],
+        ],
+      }),
+      nodeResolve(),
+      commonjs({
+        include: /node_modules/,
+      }),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+      sizeSnapshot(),
+    ],
+  },
+  {
+    input,
+    output: {
+      file: `build/dist/${pkg.name}.min.js`,
+      format: 'umd',
+      exports: 'named',
+      name: globalName,
+      globals,
+    },
+    external: Object.keys(globals),
+    plugins: [
+      babel({
+        runtimeHelpers: true,
+        exclude: 'node_modules/**',
+        presets: ['@babel/preset-react'],
+        plugins: [
+          '@babel/plugin-proposal-class-properties',
+          [
+            'transform-react-remove-prop-types',
+            { mode: 'remove', removeImport: true },
+          ],
+          ['@babel/transform-runtime'],
+        ],
+      }),
+      nodeResolve(),
+      commonjs({
+        include: /node_modules/,
+      }),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+      terser(),
+      sizeSnapshot(),
+    ],
+  },
+]
+
+export default umd
