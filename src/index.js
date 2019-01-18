92f913646fd431f5aa18043a0e27c93659e1c003
Steve Hall <hall.steven.g@gmail.com>
2019-01-17 23:07:19 -0700

    remove comments

--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
-/* eslint-disable flowtype/require-valid-file-annotation */
-
 export { default as Animate } from './Animate'
 export { default as NodeGroup } from './NodeGroup'
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,4 +1,3 @@
-// @flow weak
 /* eslint-env mocha */
 
 /**
