705fee5bc3b4c2f7c03166b0ed4932f2abe7abd0
Steve Hall <hall.steven.g@gmail.com>
2019-01-21 15:27:55 -0700

    remove transition

--- src/core/transition/index.js
+++
@@ -1,2 +0,0 @@
-export { default as transition } from './transition'
-export { default as stop } from './stop'
--- src/core/transition/stop.js
+++
@@ -1,9 +0,0 @@
-export default function stop() {
-  const ts = this.TRANSITION_SCHEDULES
-
-  if (ts) {
-    Object.keys(ts).forEach((s) => {
-      ts[s].timer.stop()
-    })
-  }
-}
