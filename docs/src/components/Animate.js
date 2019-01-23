feafe0961f21be5188df46c09e52f48485c26732
Steve Hall <hall.steven.g@gmail.com>
2019-01-23 14:06:26 -0700

    add docs NodeGroup, Animate

---
+++ docs/src/components/Animate.js
@@ -0,0 +1,10 @@
+import createAnimate from 'react-move'
+import { interpolate, interpolateTransformSvg } from 'd3-interpolate'
+
+export default createAnimate(function getInterpolator(attr, begValue, endValue) {
+  if (attr === 'transform') {
+    return interpolateTransformSvg(begValue, endValue)
+  }
+
+  return interpolate(begValue, endValue)
+})
---
+++ docs/src/components/NodeGroup.js
@@ -0,0 +1,10 @@
+import createNodeGroup from 'react-move'
+import { interpolate, interpolateTransformSvg } from 'd3-interpolate'
+
+export default createNodeGroup(function getInterpolator(attr, begValue, endValue) {
+  if (attr === 'transform') {
+    return interpolateTransformSvg(begValue, endValue)
+  }
+
+  return interpolate(begValue, endValue)
+})
