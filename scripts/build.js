31896f148e57069da035c57695a5b4f283d12444
Steve Hall <hall.steven.g@gmail.com>
2019-02-01 12:40:26 -0700

    remove comments

--- scripts/build.js
+++ scripts/build.js
@@ -5,12 +5,12 @@ const execSync = require('child_process').execSync
 
 console.log('Building CJS modules...')
 execSync(
-  'cross-env BABEL_ENV=cjs babel src -d build --ignore "src/**/*.spec.js"',
+  'cross-env BABEL_ENV=cjs babel src -d build --ignore "src/**/*.spec.js" --no-comments',
 )
 
 console.log('Building ESM modules...')
 execSync(
-  'cross-env BABEL_ENV=esm babel src -d build/es --ignore "src/**/*.spec.js"',
+  'cross-env BABEL_ENV=esm babel src -d build/es --ignore "src/**/*.spec.js" --no-comments',
 )
 
 console.log('Building UMD files...')
