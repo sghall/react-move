6197300356cdc3e1c190ad3c1198a938a13c2b6a
Steven Hall <sghall@protonmail.com>
2021-06-12 20:03:16 -0700

    update deps

--- test/utils/init.js
+++ test/utils/init.js
@@ -1,4 +1,4 @@
 import enzyme from 'enzyme/build/index'
-import Adapter from 'enzyme-adapter-react-16'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 
 enzyme.configure({ adapter: new Adapter() })
